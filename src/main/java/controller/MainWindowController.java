8a59dbaf70da7ead72f0baf74d35e6a71ceffd3d
githubmage2002 <pasinduchithakaa17@gmail.com>
2024-10-23 22:33:16 +0530

    chek

---
+++ src/main/java/controller/MainWindowController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MainWindowController {
+
+    @FXML
+    private AnchorPane anchorBody;
+
+    @FXML
+    private AnchorPane anchorHed;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+}
